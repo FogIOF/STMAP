a3c123bc52dcba6d2a9b029412fd8d235db59d2a
FogIOF <70043840+FogIOF@users.noreply.github.com>
2024-11-01 12:17:08 +1000

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -37,5 +37,10 @@ Endpoint = ${peer_endpoint}
 EOM
 )
 
+echo -e "\n\n\n"
+[ -t 1 ] && echo "########## НАЧАЛО КОНФИГА ##########"
+echo "${conf}"
+[ -t 1 ] && echo "########### КОНЕЦ КОНФИГА ###########"
+
 conf_base64=$(echo -n "${conf}" | base64 -w 0)
-echo "Скачать конфиг файлом: https://fogiof.github.io/STMAP/?filename=WG.conf&content=${conf_base64}"
+echo "Скачать конфиг файлом: https://immalware.github.io/downloader.html?filename=WARP.conf&content=${conf_base64}"
