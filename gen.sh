23067763f364589248436cbdc0238dde848039ca
FogIOF <70043840+FogIOF@users.noreply.github.com>
2024-11-01 11:58:16 +1000

    Add files via upload

---
+++ gen.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+clear
+mkdir -p ~/.cloudshell && touch ~/.cloudshell/no-apt-get-warning
+echo "Установка зависимостей..."
+apt update -y && apt install sudo -y
+sudo apt-get update -y --fix-missing && sudo apt-get install wireguard-tools jq wget -y --fix-missing
+
+priv="${1:-$(wg genkey)}"
+pub="${2:-$(echo "${priv}" | wg pubkey)}"
+api="https://api.cloudflareclient.com/v0i1909051800"
+ins() { curl -s -H 'user-agent:' -H 'content-type: application/json' -X "$1" "${api}/$2" "${@:3}"; }
+sec() { ins "$1" "$2" -H "authorization: Bearer $3" "${@:4}"; }
+response=$(ins POST "reg" -d "{\"install_id\":\"\",\"tos\":\"$(date -u +%FT%T.000Z)\",\"key\":\"${pub}\",\"fcm_token\":\"\",\"type\":\"ios\",\"locale\":\"en_US\"}")
+
+clear
+id=$(echo "$response" | jq -r '.result.id')
+token=$(echo "$response" | jq -r '.result.token')
+response=$(sec PATCH "reg/${id}" "$token" -d '{"warp_enabled":true}')
+peer_pub=$(echo "$response" | jq -r '.result.config.peers[0].public_key')
+peer_endpoint=$(echo "$response" | jq -r '.result.config.peers[0].endpoint.host')
+client_ipv4=$(echo "$response" | jq -r '.result.config.interface.addresses.v4')
+client_ipv6=$(echo "$response" | jq -r '.result.config.interface.addresses.v6')
+
+conf=$(cat <<-EOM
+[Interface]
+PrivateKey = ${priv}
+Address = ${client_ipv4}, ${client_ipv6}
+DNS = 1.1.1.1, 8.8.8.8
+
+[Peer]
+PublicKey = ${peer_pub}
+AllowedIPs = 0.0.0.0/0
+Endpoint = ${peer_endpoint}
+EOM
+)
+
+conf_base64=$(echo -n "${conf}" | base64 -w 0)
+echo "Скачать конфиг: https://fogiof.github.io/STMAP/?filename=WG.conf&content=${conf_base64}"
